66103950a15141743e74f27d0784e49274db6a19
ahmed93 <hero.20112012@gmail.com>
2014-09-22 12:45:17 +0300

    adding products/js

--- view/javascript/index.js
+++ view/javascript/index.js
@@ -88,45 +88,7 @@ function checkPass() {
  *	Return : none
  *	Author: Ahmed Mohmaed Magdi
  */
-function getProducts() {
-	$.ajax({
-		type: "POST",
-		url: "http://localhost/AL/controller/products.php",
-		success: function(data){
-			var array = $.parseJSON(data);
-			var productHTML = "<div class=\"row well\">";
 
-			for (var i = 0; i < array.length; i++) {
-				var id = array[i]['ID'];
-				var description = array[i]['description']||"No info";
-				var img = array[i]['img'];
-				var name = array[i]['name'];
-				var price = array[i]['price'];
-				var stock = array[i]['stock'];
-
-				productHTML +=" <div class=\"product-cell col-md-2 container\"> \
-									<div class=\"product-img well row\"> \
-										<img class=\"image-responsive\" src=\""+array[i]['img']+"\" alt=\""+array[i]['name']+"\"> \
-									</div> \
-									<div class=\"product-des row\"> \
-										<div class=\"well\"> \
-											<font>"+description+"</font> \
-										</div> \
-									</div> \
-									<div class=\"product-opt well row\"> \
-										<div class=\"col-md-4\"> \
-											<button name=\"product_"+array[i]['ID']+"\" type=\"submit\" class=\"btn btn-primary\">buy</button> \
-										</div> \
-										<div class=\"col-md-offset-3 col-md-4\"> \
-											<p>"+array[i]['price']+"EGP</p> \
-										</div> \
-									</div> \
-								</div>";
-			};
-			$("#main-body").html(productHTML);
-		}
-	});
-}
 
 function validate(firstN, lastN, EM, Pass, PassC) {
 	var x = false;
