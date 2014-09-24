00246e0a9adc094fd8c218e814e4186405637d91
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2014-09-24 21:35:57 +0300

    Fixing edit in user profile

--- controller/editUser.php
+++ controller/editUser.php
@@ -1,6 +1,19 @@
 <?php session_start(); ?>
 <?php
-function editUser($first_name, $last_name, $email, $password, $avatar)
+function editUser($first_name, $last_name, $email, $password, $avatar , $check)
+{
+	switch ($check) {
+		case 'true':
+			editUser_with_password($first_name, $last_name, $email, $password, $avatar);
+			break;
+		
+		case 'false':
+			editUser_with_no_password($first_name, $last_name, $email, $avatar);
+			break;
+	}
+}
+
+fuction editUser_with_password($first_name, $last_name, $email, $password, $avatar)
 {
 	$con=mysqli_connect("localhost","root","root","eshop");
 	// Check connection
@@ -18,5 +31,27 @@ function editUser($first_name, $last_name, $email, $password, $avatar)
 	 		email = '$email', password = '$password', avatar = '$avatar' WHERE ID='$_SESSION[U_ID]'"); 
 	}
 	mysqli_close($con);
+}
+
+
+fuction editUser_with_no_password($first_name, $last_name, $email, $avatar)
+{
+	$con=mysqli_connect("localhost","root","root","eshop");
+	// Check connection
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	if($avatar == "")
+	{
+		$query = mysqli_query($con, "UPDATE User SET first_name = '$first_name', last_name = '$last_name', 
+	 			email = '$email' WHERE ID='$_SESSION[U_ID]"); 
+	}
+	else
+	{	
+		$query = mysqli_query($con, "UPDATE User SET first_name = '$first_name', last_name = '$last_name', 
+	 		email = '$email', password = '$password', avatar = '$avatar' WHERE ID='$_SESSION[U_ID]'"); 
+	}
+	mysqli_close($con);
+}
 }
 ?>
